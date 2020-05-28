f3c04504d9eec313bfb6bb107e32f040eb8b53c6
Egor <ze99@rambler.ru>
2020-05-28 12:19:52 +0300

    some changes

--- src/main/java/com/program/nfa/Test.java
+++ src/main/java/com/program/nfa/Test.java
@@ -13,17 +13,17 @@ public class Test {
 //        converter.printEquivalentNFA();
 //    }
 
-//    public static void main(String[] args) {
-//        Converter converter = new Converter(new char[]{'a', 'b', 'e'}, 3, System.out, 1, 1, new int[]{3});
-//        converter.insertIntoTransitionTable(1, 'a', 1);
-//        converter.insertIntoTransitionTable(1, 'e', 2);
-//        converter.insertIntoTransitionTable(2, 'b', 2);
-//        converter.insertIntoTransitionTable(2, 'e', 3);
-//        converter.insertIntoTransitionTable(3, 'a', 3);
-//        converter.insertIntoTransitionTable(3, 'b', 3);
-//
-//        converter.printEquivalentNFA();
-//    }
+    public static void main(String[] args) {
+        Converter converter = new Converter(new char[]{'a', 'b', 'e'}, 3, System.out, 1, 1, new int[]{3});
+        converter.insertIntoTransitionTable(1, 'a', 1);
+        converter.insertIntoTransitionTable(1, 'e', 2);
+        converter.insertIntoTransitionTable(2, 'b', 2);
+        converter.insertIntoTransitionTable(2, 'e', 3);
+        converter.insertIntoTransitionTable(3, 'a', 3);
+        converter.insertIntoTransitionTable(3, 'b', 3);
+
+        converter.printEquivalentNFA();
+    }
 
 //    public static void main(String[] args) {
 //        Converter converter = new Converter(new char[]{'a','b','e'},5, System.out , 1,1, new int[]{5});
